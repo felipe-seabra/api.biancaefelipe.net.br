41d5d9ed4fd859e58e60204a352881d6b1843f62
Felipe Seabra <feliperoce@gmail.com>
2024-02-08 23:44:56 -0300

    feat: inser password modify

--- src/middlewares/schema.ts
+++ src/middlewares/schema.ts
@@ -14,6 +14,7 @@ export const updateUserSchema = Joi.object({
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required(),
+  password: Joi.string().min(6),
   isAdmin: Joi.boolean().required(),
 });
 
